154b5848dd590561c233de0953f09bde41b6ba39
Hardel-DW <hardel7401@gmail.com>
2022-11-15 19:17:48 +0100

    The component Switch respect convention

--- components/form/Switch/index.tsx
+++ components/form/Switch/index.tsx
@@ -1,5 +1,5 @@
 import React, { useId } from 'react';
-import styles from './Switch.module.scss';
+import styles from './styles.module.scss';
 
 type Props = {
     onClick?: (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => void;
