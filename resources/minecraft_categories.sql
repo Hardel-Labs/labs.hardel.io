0b9b4f8410ad2cf8580f0c4305e71f8ddcccd888
Hardel-DW <hardel7401@gmail.com>
2022-11-17 05:32:11 +0100

    Categories resources

---
+++ resources/minecraft_categories.sql
@@ -0,0 +1,12 @@
+INSERT INTO `Category` (`name`, `asset`) VALUES
+('Building Blocks', 'vanilla/bricks.webp'),
+('Colored Blocks', 'vanilla/cyan_wool.webp'),
+('Natural Blocks', 'vanilla/grass_block.webp'),
+('Functionnal Blocks', 'vanilla/oak_sign.webp'),
+('Redstone Blocks', 'vanilla/redstone.webp'),
+('Tools & Utilities', 'vanilla/diamond_pickaxe.webp'),
+('Combat', 'vanilla/netherite_sword.webp'),
+('Food & Drinks', 'vanilla/golden_apple.webp'),
+('Ingredients', 'vanilla/golden_apple.webp'),
+('Spawn Eggs', 'vanilla/pig_spawn_egg.webp'),
+('Operator Utilities', 'vanilla/command_block.webp');
