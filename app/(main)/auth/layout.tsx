72631c04c345b8cd99eb97b0eb6cd0deff8d87a3
Hardel-DW <hardel7401@gmail.com>
2022-11-24 21:45:27 +0100

    Auth page removed useless layout.

--- app/(main)/auth/layout.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-// This file patch a bug on deployment
-export default async function AuthLayout({ children }: { children: React.ReactNode }) {
-    return <>{children}</>;
-}
--- app/(main)/auth/page.tsx
+++ app/(main)/auth/page.tsx
@@ -1,7 +1,7 @@
 import { unstable_getServerSession } from 'next-auth/next';
 import { authOptions } from '@session';
 
-export default async function Page() {
+export default async function AuthPage() {
     const session = await unstable_getServerSession(authOptions);
 
     return (
