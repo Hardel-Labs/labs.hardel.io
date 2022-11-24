ae7a82e4733f47ade13af73b9289cc38eb7f9f3c
Hardel-DW <hardel7401@gmail.com>
2022-11-24 21:46:07 +0100

    Header fix

--- app/(admin)/Header.tsx
+++ app/(admin)/Header.tsx
@@ -20,7 +20,7 @@ export default function Header(props: Props) {
 
             <button type="button" className="w-8 h-8 flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300" id="user-menu-button">
                 <span className="sr-only">Open user menu</span>
-                <Image className="rounded-full" width={32} height={32} src={props.data?.user.image ?? Harion} alt="user photo" />
+                <Image className="rounded-full" width={32} height={32} src={props.data?.user?.image ?? Harion} alt="user photo" />
             </button>
         </nav>
     );
