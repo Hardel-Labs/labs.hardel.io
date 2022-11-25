3459fb58fc0c55ac3b652e54b5482ad39a33dd80
Hardel-DW <hardel7401@gmail.com>
2022-11-26 00:30:08 +0100

    Use CLX function in button

--- components/form/Button/White.tsx
+++ components/form/Button/White.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { clx } from '@libs/utils';
 
 export default function WhiteButton(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
-    return <button {...props} className={'px-8 hover:scale-90 transition-transform py-2 rounded-md bg-white text-black font-semibold border-gray-600 border ' + props.className} />;
+    return (
+        <button {...props} className={clx('px-8 hover:scale-90 transition-transform py-2 rounded-md bg-white text-black font-semibold border-gray-600 border', props.className)} />
+    );
 }
