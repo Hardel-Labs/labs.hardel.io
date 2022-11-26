b54c75423d3571bb822c85f9c81d5eeb8fd7ddef
Hardel-DW <hardel7401@gmail.com>
2022-11-26 05:13:07 +0100

    Rainbow button use clx

--- components/form/Button/Rainbow/index.tsx
+++ components/form/Button/Rainbow/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './styles.module.scss';
+import { clx } from '@libs/utils';
 
 export default function RainbowButton(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
-    return <button {...props} className={styles.button + ' px-8 hover:scale-90 transition-transform py-2 rounded-md bg-black text-white font-semibold ' + props.className} />;
+    return <button {...props} className={clx(styles.button, 'px-8 hover:scale-90 transition-transform py-2 rounded-md bg-black text-white font-semibold', props?.className)} />;
 }
