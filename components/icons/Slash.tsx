9091b00e6fa69d3320dc524183a553db869ea002
Hardel-DW <hardel7401@gmail.com>
2022-11-26 05:12:54 +0100

    Added new icons

---
+++ components/icons/Slash.tsx
@@ -0,0 +1,10 @@
+import React, { SVGProps } from 'react';
+
+export function Slash(props: SVGProps<SVGSVGElement>) {
+    return (
+        <svg viewBox="0 0 24 24" {...props}>
+            <path d="M16.88 3.549L7.12 20.451"></path>
+        </svg>
+    );
+}
+export default Slash;
