4a8cba32805ffb3fb8ff0b6ad5db83bf1e809aeb
Hardel-DW <hardel7401@gmail.com>
2022-11-26 05:06:27 +0100

    User dashboard is protected by middleware

--- middleware.ts
+++ middleware.ts
@@ -3,18 +3,25 @@ import { NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
 export async function middleware(req: NextRequest) {
-    if (req.nextUrl.pathname.startsWith('/dashboard')) {
+    if (req.nextUrl.pathname.startsWith('/admin/dashboard')) {
         const token = await getToken({ req, secret: process.env.SECRET });
         if (!token) {
-            return NextResponse.redirect('/api/auth/signin');
+            return NextResponse.redirect(new URL('/', req.url));
         }
 
         if (token.userData?.roles !== 'ADMIN') {
             return NextResponse.redirect(new URL('/', req.url));
         }
     }
+
+    if (req.nextUrl.pathname.startsWith('/dashboard')) {
+        const token = await getToken({ req, secret: process.env.SECRET });
+        if (!token) {
+            return NextResponse.redirect(new URL('/', req.url));
+        }
+    }
 }
 
 export const config = {
-    matcher: '/dashboard/admin/:path*'
+    matcher: ['/admin/dashboard/:path*', '/dashboard/:path*']
 };
