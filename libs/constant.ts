eab87687beddb609a924d948f3b95409cf396cc2
Hardel-DW <hardel7401@gmail.com>
2022-11-30 08:07:28 +0100

    The function that transforms date into elapsed time has been simplified and is more complete.

--- libs/constant.ts
+++ libs/constant.ts
@@ -32,28 +32,39 @@ export const DEFAULT_SLOT_VALUE: SlotData[] = [{ id: 'crafting:result', count: 1
 
 export const AGO_SINCE = [
     {
+        name: 'second',
+        value: 0,
+        suffix: 's'
+    },
+    {
         name: 'minutes',
-        value: 60
+        value: 60,
+        suffix: 'm'
     },
     {
         name: 'hours',
-        value: 60 * 60
+        value: 60 * 60,
+        suffix: 'h'
     },
     {
         name: 'days',
-        value: 60 * 60 * 24
+        value: 60 * 60 * 24,
+        suffix: 'd'
     },
     {
         name: 'weeks',
-        value: 60 * 60 * 24 * 7
+        value: 60 * 60 * 24 * 7,
+        suffix: 'w'
     },
     {
         name: 'months',
-        value: 60 * 60 * 24 * 30
+        value: 60 * 60 * 24 * 30,
+        suffix: 'mo'
     },
     {
         name: 'years',
-        value: 60 * 60 * 24 * 365
+        value: 60 * 60 * 24 * 365,
+        suffix: 'y'
     }
 ];
 
@@ -66,11 +77,13 @@ export const VERSION: Option[] = [
 
 export const ROLES = [
     {
-        value: 'admin',
+        value: 'ADMIN',
         name: 'Admin'
     },
     {
-        value: 'user',
+        value: 'USER',
         name: 'User'
     }
 ];
+
+export const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
--- libs/utils.ts
+++ libs/utils.ts
@@ -1,3 +1,5 @@
+import { AGO_SINCE } from '@libs/constant';
+
 const randomString = (length: number) => Array.from(Array(length), () => Math.floor(Math.random() * 36).toString(36)).join('');
 
 const removeSpaces = (str: string) => str.replace(/\s/g, '');
@@ -5,32 +7,27 @@ const removeSpecialCharacters = (str: string) => str.replace(/[^a-zA-Z_]/gi, '')
 const removeSpacesAndSpecialCharacters = (str: string) => removeSpecialCharacters(removeSpaces(str));
 const clx = (...args: (string | undefined)[]) => args.filter(Boolean).join(' ');
 
+/**
+ * Return object with the suffix and the value
+ * @param date
+ */
 const timeSince = (date: Date) => {
-    if (!date) return 'Unknown';
+    let element = AGO_SINCE[AGO_SINCE.length - 1];
+    const seconds = Math.floor((new Date().getTime() - (date?.getTime?.() ?? 0)) / 1000);
+    for (let i = 0; i < AGO_SINCE.length; i++) {
+        const value = AGO_SINCE[i].value;
+        if (seconds < value) {
+            element = AGO_SINCE[i - 1];
+            break;
+        }
+    }
 
-    const seconds = Math.floor((new Date().getTime() - new Date(date).getTime()) / 1000);
-    let interval = seconds / 31536000;
-
-    if (interval > 1) {
-        return Math.floor(interval) + ' years ago';
-    }
-    interval = seconds / 2592000;
-    if (interval > 1) {
-        return Math.floor(interval) + ' months ago';
-    }
-    interval = seconds / 86400;
-    if (interval > 1) {
-        return Math.floor(interval) + ' days ago';
-    }
-    interval = seconds / 3600;
-    if (interval > 1) {
-        return Math.floor(interval) + ' hours ago';
-    }
-    interval = seconds / 60;
-    if (interval > 1) {
-        return Math.floor(interval) + ' minutes ago';
-    }
-    return Math.floor(seconds) + ' seconds ago';
+    const time = Math.floor(seconds / element.value);
+    return {
+        name: element.name,
+        suffix: element.suffix,
+        value: time === Infinity ? seconds : time
+    };
 };
 
 export { randomString, removeSpacesAndSpecialCharacters, clx, timeSince };
