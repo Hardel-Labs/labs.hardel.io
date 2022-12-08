a575a88193f133b4970cbc11fc04e11ec029a96e
Hardel-DW <hardel7401@gmail.com>
2022-12-08 03:59:49 +0100

    Rainbow button refactor

--- components/form/Button/Rainbow.tsx
+++ components/form/Button/Rainbow.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styles from './styles.module.scss';
 import { clx } from '@libs/utils';
 
 export default function RainbowButton(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
@@ -7,8 +6,7 @@ export default function RainbowButton(props: React.ButtonHTMLAttributes<HTMLButt
         <button
             {...props}
             className={clx(
-                styles.button,
-                'px-8 hover:scale-90 transition-transform py-2 rounded-md bg-black text-white font-semibold',
+                'rainbow-border px-8 hover:scale-90 transition-transform py-2 rounded-md bg-black text-white font-semibold',
                 'disabled:opacity-50 disabled:cursor-not-allowed',
                 props?.className
             )}
